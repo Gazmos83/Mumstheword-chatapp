784b452bb41c56ec4809f2bd659aeeb1e7012631
gazmos83 <chimericano@gmail.com>
2019-04-10 10:47:07 +0200

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,7 @@ class User < ApplicationRecord
    update_attribute(:remember_digest, User.digest(remember_token))
  end
 
+ # Returns true if the given token matches the digest.
  def authenticated?(remember_token)
    return false if remember_digest.nil?
    BCrypt::Password.new(remember_digest).is_password?(remember_token)
@@ -39,6 +40,12 @@ class User < ApplicationRecord
  def forget
    update_attribute(:remember_digest, nil)
  end
+ # Returns true if the given token matches the digest.
+ def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
 
 
 end

670394fcf6c9ceb5959e9adaa48d8543034afb10
gazmos83 <chimericano@gmail.com>
2019-04-19 21:48:58 +0200

    user-microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,4 +24,13 @@ User.create!(name:  "Pingu",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+               users = User.order(:created_at).take(6)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end

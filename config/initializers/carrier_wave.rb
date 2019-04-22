32aee361f853b39a42ef4581bcfc734e3de9d33f
gazmos83 <chimericano@gmail.com>
2019-04-22 09:11:13 +0200

    add user microposts

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,11 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Configuration for MEGA
-      :provider              => 'MEGA',
-      :user_name    => ENV['S3_ACCESS_KEY'],
-      :user_password => ENV['S3_SECRET_KEY']
-    }
-    config.fog_directory     =  ENV['S3_BUCKET']
-  end
-end
+#if Rails.env.production?
+#  CarrierWave.configure do |config|
+#    config.fog_credentials = {
+#      # Configuration for MEGA
+#      :provider              => 'MEGA',
+#      :key    => ENV['MEGA_ACCESS_KEY'],
+#      :user_password => ENV['MEGA_PASSWORD']
+#    }
+#    config.fog_directory     =  ENV['S3_BUCKET']
+#  end
+#end
